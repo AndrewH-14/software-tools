ed2ce2a56485913c49fb29d9754c0a80c56248d5
Andrew Hankins <achankins@crimson.ua.edu>
2021-08-09 17:38:36 -0500

    Worked on setting up directories.

---
+++ Documentation/plamtuml/run_plantuml.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run plantuml to regenerate all the diagrams in this directory
+java -jar ../../../../doc/plantuml.1.2021.8.jar ./
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tools
+
 Tools for coding projects.
+
+## Directory Tree
+
+* Docker
+* Documentation
